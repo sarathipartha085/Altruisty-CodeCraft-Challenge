60de816b2bdbbd29e95f2e05f0c04e444383d44d
PARTHASARATHI M <sarathipartha085@gmail.com>
2024-11-10 17:58:40 +0530

    Create Question-05.cpp

---
+++ Altruisty-CodeCraft-Challenge/Question-05.cpp
@@ -0,0 +1,56 @@
+/*
+
+Question 5
+
+The Selection of candidates for ZSCS includes a strength test conducted on the field. A group of 4 trainees is participating in a lifting test for 3 rounds. After each round, you will record the strength level (in kilograms) lifted by each trainee. After all trainees complete all rounds, calculate the average strength level for each trainee across the 3 rounds, and identify the trainee (or trainees) with the highest average strength level as the strongest. If multiple trainees attain the same highest average level, they should all be selected.
+
+Display the strongest trainee (or trainees) and the highest average strength level.
+
+Note:
+
+Only accept strength values between 1 and 200. If a strength value outside this range is entered, output "INVALID INPUT".
+If the calculated highest average strength of all trainees is below 100, declare the trainees as unfit with the message "All trainees are unfit."
+Round the average strength values to the nearest integer.
+
+Example 1:
+
+INPUT VALUES:
+
+150
+160
+155
+158
+140
+130
+125
+142
+160
+135
+125
+140
+
+
+OUTPUT VALUES:
+
+Trainee Number : 1
+Trainee Number : 3
+
+In this scenario, enter 12 integer values representing strength levels for each trainee, organized by:
+
+Round 1
+  Strength level of Trainee 1
+  Strength level of Trainee 2
+  Strength level of Trainee 3
+  Strength level of Trainee 4
+Round 2
+  Strength level of Trainee 1
+  Strength level of Trainee 2
+  Strength level of Trainee 3
+  Strength level of Trainee 4
+Round 3
+  Strength level of Trainee 1
+  Strength level of Trainee 2
+  Strength level of Trainee 3
+  Strength level of Trainee 4
+
+*/
